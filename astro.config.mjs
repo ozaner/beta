bc6a4ef59607773670a0009b69711bc2c253d57f
ozaner <56015962+ozaner@users.noreply.github.com>
2023-05-30 23:01:39 -0400

    Configure Github Pages deployment
    Github should auto deploy the site after every commit, or manually.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ozaner.github.io/beta',
+  base: '/beta'
+});
